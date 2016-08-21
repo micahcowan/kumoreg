a94b7b2de037283c4632f95e8ec63e7be8b627ad
Micah Cowan <micah@addictivecode.org>
2016-08-21 13:39:22 -0700

    Farmed out single till report for future use in full all-tills report.

--- src/main/java/org/kumoricon/site/utility/closeouttill/CloseOutTillPresenter.java
+++ src/main/java/org/kumoricon/site/utility/closeouttill/CloseOutTillPresenter.java
@@ -5,13 +5,13 @@ import org.kumoricon.model.order.OrderRepository;
 import org.kumoricon.model.user.User;
 import org.kumoricon.model.user.UserRepository;
 import org.kumoricon.service.print.ReportPrintService;
+import org.kumoricon.site.report.till.TillReportPresenter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -30,38 +30,27 @@ public class CloseOutTillPresenter {
 
     public void closeTill(CloseOutTillView view, User currentUser) {
         if (currentUser != null) {
-            log.info("{} closing out till, session number {}", currentUser, currentUser.getSessionNumber());
+            Integer sessionNumber = currentUser.getSessionNumber();
+            log.info("{} closing out till, session number {}", currentUser, sessionNumber);
             StringBuilder output = new StringBuilder();
-            output.append(String.format("User ID: %d (%s)\n", currentUser.getId(), currentUser.getUsername()));
-            output.append(String.format("%s %s\n", currentUser.getFirstName(), currentUser.getLastName()));
-            output.append(String.format("%s\n", LocalDateTime.now()));
-            output.append("--------------------------------------------------------------------------------\n");
-            output.append(String.format("Session Number: %d\n\n", currentUser.getSessionNumber()));
-
             List<Object[]> results = orderRepository.getSessionOrderCountsAndTotals(
-                    currentUser.getId(), currentUser.getSessionNumber());
-            output.append(String.format("%-40s\t%s\t%s\n", "Payment Type", "Count", "Total"));
+                    currentUser.getId(), sessionNumber);
+            String report = TillReportPresenter.getTillReportStr(currentUser, sessionNumber, results);
+            output.append(report);
+
             for (Object[] line : results) {
-                output.append(String.format("%-40s\t%5d\t$%8.2f\n",
-                        getPaymentType((Integer)line[0]), line[1], line[2]));
                 log.info("{} till session {}: Payment Type: {}, Attendees: {}, Total: ${}",
-                        currentUser, currentUser.getSessionNumber(), getPaymentType((Integer)line[0]),
+                        currentUser, sessionNumber, Order.PaymentType.fromInteger((Integer)line[0]).toString(),
                         line[1], line[2]);
             }
-            output.append("--------------------------------------------------------------------------------\n");
 
-            currentUser.setSessionNumber(currentUser.getSessionNumber() + 1);
+            sessionNumber += 1;
+            currentUser.setSessionNumber(sessionNumber);
             userRepository.save(currentUser);
-            output.append("Session closed. New session number is: ");
-            output.append(currentUser.getSessionNumber());
+            output.append(String.format("Session closed. New session number is: %d", sessionNumber));
             view.showData(output.toString());
             log.info("{} created new till session, number {}.", currentUser, currentUser.getSessionNumber());
             view.notify(reportPrintService.printReport(output.toString(), view.getCurrentClientIPAddress()));
         }
-    }
-
-    private static String getPaymentType(Integer typeId) {
-        Order.PaymentType[] orderTypes = Order.PaymentType.values();
-        return orderTypes[typeId].toString();
     }
 }
